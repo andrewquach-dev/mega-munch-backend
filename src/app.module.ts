fa6243e1b38c810549c4134ae6a83669c2559345
Andrew Quach <andrewrquach@gmail.com>
2022-11-04 16:46:00 -0700

    added class-validator for dto and added typeorm module for postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,26 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'sasquach',
+      password: 'banhmi123',
+      database: 'mega-munch',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
